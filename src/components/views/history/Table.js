43b2bba129ca46b49ecf9d95c5402130782cabfe
VukadinovicJS <jano.vuk@hotmail.com>
2023-04-03 21:29:00 +0200

    #28

--- src/components/views/history/Table.js
+++ src/components/views/history/Table.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { IconButton } from '@mui/material';
-import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
+import React from "react";
+import { IconButton } from "@mui/material";
+import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
-
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: '#2196f3',
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Table = ({ data }) => {
+  const [open, setOpen] = React.useState(false);
+  const [id, setID] = React.useState("");
+  const handleOpen = (id) => {
+    setID(id);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
   return (
     <table>
       <thead>
@@ -20,8 +41,34 @@ const Table = ({ data }) => {
           <tr key={item.id}>
             <td>{item.id}</td>
             <td>{item.date}</td>
-            <td>{item.correct}/{item.wrong}({(item.correct/item.wrong)*100}%)</td>
-            <td> <IconButton title="Get Info on Game" color="primary" ><ArrowDropDownCircleIcon/></IconButton></td>
+            <td>
+              {item.correct}/{item.wrong}({(item.correct / item.wrong) * 100}%)
+            </td>
+            <td>
+              {" "}
+              <IconButton
+                title={item.id}
+                color="primary"
+                onClick={() => handleOpen(item.id)}
+              >
+                <ArrowDropDownCircleIcon />
+              </IconButton>
+            </td>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box color="primary" sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  {id}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  This is where the the game info will go
+                </Typography>
+              </Box>
+            </Modal>
           </tr>
         ))}
       </tbody>
