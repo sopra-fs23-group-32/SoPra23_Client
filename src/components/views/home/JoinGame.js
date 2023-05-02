82bdd0e4b1475009d558ce371193fe588e88931d
awhoa <said.hajiabukar@uzh.ch>
2023-05-02 13:26:43 +0200

    joingamepage and multiplayergamepage

---
+++ src/components/views/home/JoinGame.js
@@ -0,0 +1,145 @@
+import React, { useEffect } from "react";
+import { api, } from "helpers/api";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { Button } from "components/ui/Button";
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    InputLabel,
+    Select,
+    MenuItem,
+    Container,
+} from "@mui/material";
+
+import "styles/views/home/Join.scss";
+
+const JoinGame = () => {
+    const history = useHistory();
+    const [openServers, setOpenServers] = useState([]);
+
+    useEffect(() => {
+        const fetchGamedata = async () => {
+            const gameInfosResponse = await api.get("/games/");
+            const gameInfos = gameInfosResponse.data;
+            setOpenServers(gameInfos);
+        };
+        fetchGamedata();
+    }, []);
+
+    const fetchQuestions = async (severInfo) => {
+        try {
+            const response1 = await api.get(`games/${severInfo.gameId}/questions`);
+            const question = response1.data;
+            const cityNamesString = JSON.stringify([
+                question.option1,
+                question.option2,
+                question.option3,
+                question.option4,
+            ]);
+            localStorage.setItem("citynames2", cityNamesString);
+            localStorage.setItem("PictureUrl", question.pictureUrl);
+            localStorage.setItem("CorrectOption", question.correctOption);
+            
+            return question;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    const joinServer = async serverInfo => {
+        // fetchQuestions(serverInfo);
+        localStorage.setItem("score", 0);
+        localStorage.setItem("gameId", serverInfo.gameId);
+        localStorage.setItem("totalRounds", serverInfo.gameRounds);
+        localStorage.setItem("roundNumber", 0);
+        localStorage.setItem("category", serverInfo.category);
+        localStorage.setItem("gamePlayer", serverInfo.playerNum)
+        localStorage.setItem("isServer", 0);
+        
+        history.push("/StartGamePage");
+    }
+
+    return (
+        <Container className="joinboard container">
+            <div className="headerrow">
+                <h2>Join the Server</h2>
+            </div>
+            <div className="joinboard field">
+                <div className="sever-field">
+                    <TableContainer
+                        component={Paper}
+                        sx={{ backgroundColor: "transparent" }}
+                    >
+                        <Table
+                            sx={{ minWidth: 650 }}
+                            aria-label="simple table"
+                            className="score-table"
+                        >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell align="center">
+                                        Category
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        Rounds
+                                    </TableCell>
+                                    <TableCell align="center">Join</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {openServers.map((openServer, index) => {
+                                    return (
+                                        <TableRow
+                                            key={index}
+                                            sx={{
+                                                "&:last-child td, &:last-child th":
+                                                    {
+                                                        border: 0,
+                                                    },
+                                            }}
+                                        >
+                                            <TableCell
+                                                component="th"
+                                                scope="row"
+                                            >
+                                                {index + 1}.
+                                            </TableCell>
+                                            <TableCell align="center">
+                                                {openServer.category}
+                                            </TableCell>
+                                            <TableCell align="center">
+                                                {openServer.gameRounds}
+                                            </TableCell>
+                                            <TableCell align="center">
+                                                <p onClick={() => joinServer(openServer)}>Join</p>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+                <div className="button-container">
+                    <Button width="30%" onClick={() => history.push("/home")}>
+                        Return to home
+                    </Button>
+                    <Button width="30%" onClick={() => history.push("/lobby")}>
+                        Create Server
+                    </Button>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default JoinGame;
