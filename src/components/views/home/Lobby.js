e804b916d701df2855410cbd89f3710cdbd1ac68
Dzl666 <Deng_Zilong@outlook.com>
2023-05-22 14:58:23 +0200

    fix console text

--- src/components/views/home/Lobby.js
+++ src/components/views/home/Lobby.js
@@ -36,7 +36,7 @@ const Lobby = () => {
   const handleAddPlayer = async (gameId) => {
     try {
       const response = await api.post(`/games/${gameId}/players/${userId}`);
-      console.log("Player added for Single Mode:", response.data);
+      console.log("Player added:", response.data);
     }
     catch (error) {
       toast.error(`${error.response.data.message}`);
@@ -58,8 +58,8 @@ const Lobby = () => {
 
       // Create Game
       const requestBody = {category:category_uppercase, totalRounds:gameRounds, countdownTime:gameDuration,};
-      const response = (await api.post("/games", requestBody)).data;
-      const gameId = response.gameId;
+      const response = await api.post("/games", requestBody);
+      const gameId = response.data.gameId;
       localStorage.setItem("gameId", gameId);
       console.log("Multiplayer Game created: ", response.data)
 
@@ -83,8 +83,8 @@ const Lobby = () => {
       localStorage.setItem("totalRounds", gameRounds);
       localStorage.setItem("countdownTime", gameDuration);
       const requestBody = {category:category, totalRounds:gameRounds, countdownTime:gameDuration,};
-      const response = (await api.post("/games", requestBody)).data;
-      const gameId = response.gameId;
+      const response = await api.post("/games", requestBody);
+      const gameId = response.data.gameId;
       localStorage.setItem("gameId", gameId);
       handleAddPlayer(gameId);
       console.log("SinglePlayer Game created: ", response.data)
